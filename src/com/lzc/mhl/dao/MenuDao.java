a646ea6e975ca6218b405905dfb1003a8fc08459
wx10173734 <1179382172@qq.com>
2022-10-26 20:56:35 +0800

    :sparkles:完成菜品显示功能

---
+++ src/com/lzc/mhl/dao/MenuDao.java
@@ -0,0 +1,12 @@
+package com.lzc.mhl.dao;
+
+import com.lzc.mhl.domain.Menu;
+
+/**
+ * @title: MenuDao
+ * @Author luozouchen
+ * @Date: 2022/10/26 20:45
+ * @Version 1.0
+ */
+public class MenuDao extends BasicDao<Menu>{
+}
---
+++ src/com/lzc/mhl/service/MenuService.java
@@ -0,0 +1,22 @@
+package com.lzc.mhl.service;
+
+import com.lzc.mhl.dao.MenuDao;
+import com.lzc.mhl.domain.Menu;
+
+import java.util.List;
+
+/**
+ * @title: MenuService
+ * @Author luozouchen
+ * @Date: 2022/10/26 20:45
+ * @Version 1.0
+ */
+public class MenuService {
+    private MenuDao menuDao = new MenuDao();
+
+    //显示所有菜品
+    public List<Menu> list() {
+        List<Menu> menuList = menuDao.queryMulti("select * from menu", Menu.class);
+        return menuList;
+    }
+}
